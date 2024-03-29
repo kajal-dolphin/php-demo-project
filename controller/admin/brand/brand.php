705d6eb8582e06614e84ce45657ec5a278e5c52b
kajal-dolphin <kajal@dolphinwebsolutions.com>
2024-03-29 16:43:01 +0530

    implement crud fro brand with image upload

--- controller/admin/brand/brand.php
+++ controller/admin/brand/brand.php
@@ -7,35 +7,29 @@ if(isset($_POST['save_brand'])){
         $name = $_POST['name'];
         $image = $_FILES['image']['name'];
 
-        $target_dir = "/var/www/html/e_commorce/public/admin/assets/images/brand/";
-        $target_file = $target_dir . basename($_FILES["image"]["name"]);
+        $imageName =  time() . '-' . $image;
 
-        if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
-            $sql = "INSERT INTO brands (name,image) VALUES ('{$name}', '{$image}')";
-            $query = mysqli_query($connection, $sql);
-            
-            if($query)
-            {
-                $res = [
-                    'status' => 200,
-                    'message' => 'Brand Created Successfully'
-                ];
-                echo json_encode($res);
-                return;
-            }
-            else
-            {
-                $res = [
-                    'status' => 500,
-                    'message' => 'Student Not Created'
-                ];
-                echo json_encode($res);
-                return;
-            }
-        } else {
+        $target_dir = "/var/www/html/e_commorce/public/admin/assets/images/brand/";
+        $target_file = $target_dir . basename($imageName);
+        move_uploaded_file($_FILES["image"]["tmp_name"], $target_file);
+
+        $sql = "INSERT INTO brands (name,image) VALUES ('{$name}', '{$imageName}')";
+        $query = mysqli_query($connection, $sql);
+        
+        if($query)
+        {
             $res = [
-                'status' => 400,
-                'message' => 'Error while uploading file'
+                'status' => 200,
+                'message' => 'Brand Created Successfully'
+            ];
+            echo json_encode($res);
+            return;
+        }
+        else
+        {
+            $res = [
+                'status' => 500,
+                'message' => 'Brand Not Created'
             ];
             echo json_encode($res);
             return;
@@ -45,6 +39,124 @@ if(isset($_POST['save_brand'])){
         $res = [
             'status' => 422,
             'message' => 'All fields are mandatory'
+        ];
+        echo json_encode($res);
+        return;
+    }
+}
+
+
+if(isset($_GET['brand_id']))
+{
+   $brand_id = $_GET['brand_id'];
+
+   $sql = "SELECT * FROM brands WHERE id='$brand_id'";
+   $query = mysqli_query($connection, $sql);
+
+   if(mysqli_num_rows($query) == 1)
+    {
+        $brand = mysqli_fetch_array($query);
+
+        $res = [
+            'status' => 200,
+            'message' => 'Brand View Successfully',
+            'data' => $brand
+        ];
+        echo json_encode($res);
+        return;
+    }
+    else
+    {
+        $res = [
+            'status' => 404,
+            'message' => 'Brand Not Found'
+        ];
+        echo json_encode($res);
+        return;
+    }
+}
+
+if(isset($_POST['update_brand'])){
+   if(!empty($_POST['name'])){
+        $brand_id = $_POST['edit_brand_id'];
+        $name = $_POST['name'];
+        $image = $_FILES['image']['name'];
+        $imageName = $_POST['edit_old_image'];
+
+        if(!empty($_FILES['image']['name'])){
+            $image_path = "/var/www/html/e_commorce/public/admin/assets/images/brand/" . $_POST['edit_old_image'];
+            unlink($image_path);
+
+            $imageName =  time() . '-' . $image;
+            $target_dir = "/var/www/html/e_commorce/public/admin/assets/images/brand/";
+            $target_file = $target_dir . basename($imageName);
+            move_uploaded_file($_FILES["image"]["tmp_name"], $target_file);
+        }
+
+        $sql = "UPDATE brands SET name='$name', image='$imageName' WHERE id='$brand_id'";
+        $query = mysqli_query($connection, $sql);
+        
+        if($query)
+        {
+            $res = [
+                'status' => 200,
+                'message' => 'Brand Updated Successfully'
+            ];
+            echo json_encode($res);
+            return;
+        }
+        else
+        {
+            $res = [
+                'status' => 500,
+                'message' => 'Brand Not Upadated'
+            ];
+            echo json_encode($res);
+            return;
+        }
+   }
+   else{
+        $res = [
+            'status' => 422,
+            'message' => 'All fields are mandatory'
+        ];
+        echo json_encode($res);
+        return;
+    }
+}
+
+if(isset($_POST['delete_student']))
+{
+    $brand_id =  $_POST['brand_id'];
+
+    $getBrandId = "SELECT * FROM brands WHERE id='$brand_id'";
+    $getBrandDeatil = mysqli_query($connection, $getBrandId);
+
+    if (mysqli_num_rows($getBrandDeatil) > 0) {
+        $row = mysqli_fetch_assoc($getBrandDeatil);
+        $image = $row['image'];
+
+        $image_path = "/var/www/html/e_commorce/public/admin/assets/images/brand/" . $image;
+        unlink($image_path);
+    }
+    
+    $sql = "DELETE FROM brands WHERE id='$brand_id'";
+    $query = mysqli_query($connection, $sql);
+
+    if($query)
+    {
+        $res = [
+            'status' => 200,
+            'message' => 'Brand Deleted Successfully'
+        ];
+        echo json_encode($res);
+        return;
+    }
+    else
+    {
+        $res = [
+            'status' => 500,
+            'message' => 'Brand Not Deleted'
         ];
         echo json_encode($res);
         return;
--- view/admin/brand/brand.php
+++ view/admin/brand/brand.php
@@ -48,23 +48,38 @@
                 <table id="example" class="table table-striped table-bordered" style="width:100%">
                     <thead>
                         <tr>
+                            <th>Id</th>
                             <th>Name</th>
                             <th>Image</th>
                             <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>Paul Byrd</td>
-                            <td>Chief Financial Officer (CFO)</td>
-                            <td>
-                                <div class="d-flex align-items-center gap-3 fs-6">
-                                    <a href="" class="text-primary"><i class="bi bi-eye-fill"></i></a>
-                                    <a href="" class="text-warning"><i class="bi bi-pencil-fill"></i></a>
-                                    <a href="" class="text-danger"><i class="bi bi-trash-fill"></i></a>
-                                </div>
-                            </td>
-                        </tr>
+                        <?php
+                        include('../../../config/db.php');
+
+                        $sql = "SELECT * FROM brands";
+                        $query = mysqli_query($connection, $sql);
+
+                        if (mysqli_num_rows($query) > 0) {
+                            foreach ($query as $brand) {
+                        ?>
+                                <tr>
+                                    <td><?= $brand['id'] ?></td>
+                                    <td><?= $brand['name'] ?></td>
+                                    <td>
+                                        <img src='../../../public/admin/assets/images/brand/<?= $brand['image'] ?>' width="100" height="100" />
+                                    </td>
+                                    <td>
+                                        <button class="btn btn-info viewBrand" type="button" value="<?= $brand['id']; ?>">View</button>
+                                        <button class="btn btn-warning editBrand" type="button" value="<?= $brand['id']; ?>">Edit</button>
+                                        <button class="btn btn-danger deleteBrand" type="button" value="<?= $brand['id']; ?>">Delete</button>
+                                    </td>
+                                </tr>
+                        <?php
+                            }
+                        }
+                        ?>
                     </tbody>
                 </table>
             </div>
@@ -73,7 +88,7 @@
         <!--end page main-->
 
 
-        <!-- Add Brand -->
+        <!-- Add Brand Modal -->
         <div class="modal fade" id="brandAddModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
@@ -84,7 +99,7 @@
                     <form id="saveBrand">
                         <div class="modal-body">
 
-                            <div id="errorMessage" class="alert alert-warning d-none"></div>
+                            <div id="errorMessage" class="alert alert-danger d-none"></div>
 
                             <div class="mb-3">
                                 <label for="">Name</label>
@@ -95,7 +110,10 @@
                             <div class="mb-3">
                                 <label for="">Image</label>
                                 <div>
-                                    <input type="file" name="image" class="form-control" />
+                                    <input type="file" name="image" class="form-control" onchange="imagePreview(this);" />
+                                    <div id="imagePreviewContainer" style="display: none;">
+                                        <img id="previewImage" alt="your image" height="100px" width="100px" class="pt-2">
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -109,10 +127,79 @@
         </div>
     </div>
 
+    <!-- View Brand Modal -->
+    <div class="modal fade" id="brandViewModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">View Brand</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+                <div class="modal-body">
+                    <div class="mb-3">
+                        <label for="">Name</label>
+                        <p id="view_name" class="form-control"></p>
+                    </div>
+                    <div class="mb-3">
+                        <label for="">Image</label>
+                        <p>
+                            <img src="" alt="" id="view_image" height="100px" width="100px">
+                        </p>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Edit Brand Modal -->
+    <div class="modal fade" id="brandEditModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Edit Brand</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form id="updateBrand">
+                <div class="modal-body">
+
+                    <div id="errorMessageUpdate" class="alert alert-danger d-none"></div>
+
+                    <input type="hidden" name="edit_brand_id" id="edit_brand_id" >
+                    <input type="hidden" name="edit_old_image" id="edit_old_image" >
+
+                    <div class="mb-3">
+                        <label for="">Name</label>
+                        <div>
+                            <input type="text" name="name" id="edit_name" class="form-control" />
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="">Image</label>
+                        <div>
+                            <input type="file" name="image" id="image" class="form-control" />
+                        </div>
+                        <img src="" alt="" id="edit_image" height="100" width="100" class="pt-2">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Update Brand</button>
+                </div>
+            </form>
+            </div>
+        </div>
+    </div>
+
     <?php include('../layout/foot-link.php');  ?>
     <script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/additional-methods.js"></script>
     <script>
         $(document).ready(function() {
+
+            //for create brand
             $('#saveBrand').validate({
                 errorClass: 'errors',
                 rules: {
@@ -166,13 +253,79 @@
                                 alertify.set('notifier', 'position', 'top-right');
                                 alertify.success(res.message);
 
-                                // $('#myTable').load(location.href + " #myTable");
+                                $('#example').load(location.href + " #example");
+
+                            } else if (res.status == 500) {
+                                alert(res.message);
+                            }
+                        },
+                        error: function(xhr, status, error) {
+                            console.log("in error");
+                        }
+                    });
+                }
+            });
+
+            //for update brand
+            $('#updateBrand').validate({
+                errorClass: 'errors',
+                rules: {
+                    name: {
+                        required: true,
+                        maxlength: 255,
+                    },
+                    image: {
+                        extension: "jpg|jpeg|png",
+                    },
+                },
+                messages: {
+                    name: {
+                        required: 'Please enter name.',
+                        maxLength: 'Max length is 255 character.',
+                    },
+                    image: {
+                        required: 'Image is Required.',
+                        extension: "Please upload file in these format only (jpg, jpeg, png)."
+                    },
+                },
+                highlight: function(element) {
+                    $(element).parent().addClass('text-danger')
+                },
+                unhighlight: function(element) {
+                    $(element).parent().removeClass('text-danger')
+                },
+                submitHandler: function(form) {
+                    var formData = new FormData(form);
+                    formData.append("update_brand", true);
+
+                    $.ajax({
+                        type: "POST",
+                        url: "../../../controller/admin/brand/brand.php",
+                        data: formData,
+                        processData: false,
+                        contentType: false,
+                        success: function(response) {
+                            var res = jQuery.parseJSON(response);
+                            if (res.status == 422) {
+                                $('#errorMessageUpdate').removeClass('d-none');
+                                $('#errorMessageUpdate').text(res.message);
+
+                            } else if (res.status == 200) {
+
+                                $('#errorMessageUpdate').addClass('d-none');
+                                $('#brandEditModal').modal('hide');
+                                $('#saveBrand')[0].reset();
+
+                                alertify.set('notifier', 'position', 'top-right');
+                                alertify.success(res.message);
+
+                                $('#example').load(location.href + " #example");
 
                             } else if (res.status == 500) {
                                 alert(res.message);
                             } else if (res.status == 400) {
-                                $('#errorMessage').removeClass('d-none');
-                                $('#errorMessage').text(res.message);
+                                $('#errorMessageUpdate').removeClass('d-none');
+                                $('#errorMessageUpdate').text(res.message);
 
                             }
                         },
@@ -183,6 +336,94 @@
                 }
             });
         });
+
+        //for view detail 
+        $(document).on('click','.viewBrand',function (e) {
+            e.preventDefault();
+            var brand_id = $(this).val();
+            $.ajax({
+                type: "GET",
+                url: "../../../controller/admin/brand/brand.php?brand_id=" + brand_id,
+                success: function(response) {
+                    var res = jQuery.parseJSON(response);
+                    if (res.status == 404) {
+                        alert(res.message);
+                    } else if (res.status == 200) {
+                        $('#view_name').text(res.data.name);
+                        $('#view_image').attr('src', '../../../public/admin/assets/images/brand/' + res.data.image);
+                        $('#brandViewModal').modal('show');
+                    }
+                }
+            });
+        });
+
+        //for edit detail 
+        $(document).on('click', '.editBrand',function (e) {
+            e.preventDefault();
+            var brand_id = $(this).val();
+            $.ajax({
+                type: "GET",
+                url: "../../../controller/admin/brand/brand.php?brand_id=" + brand_id,
+                success: function(response) {
+                    var res = jQuery.parseJSON(response);
+                    if (res.status == 404) {
+                        alert(res.message);
+                    } else if (res.status == 200) {
+                        $('#edit_name').val(res.data.name);
+                        $('#edit_brand_id').val(res.data.id);
+                        $('#edit_old_image').val(res.data.image);
+                        $('#edit_image').attr('src', '../../../public/admin/assets/images/brand/' + res.data.image);
+                        $('#brandEditModal').modal('show');
+                    }
+                }
+            });
+        });
+
+
+        //for delete brand 
+        $(document).on('click', '.deleteBrand', function (e) {
+            e.preventDefault();
+
+            if(confirm('Are you sure you want to delete this data?'))
+            {
+                var brand_id = $(this).val();
+                $.ajax({
+                    type: "POST",
+                    url: "../../../controller/admin/brand/brand.php",
+                    data: {
+                        'delete_student': true,
+                        'brand_id': brand_id
+                    },
+                    success: function (response) {
+
+                        var res = jQuery.parseJSON(response);
+                        if(res.status == 500) {
+
+                            alert(res.message);
+                        }else{
+                            alertify.set('notifier','position', 'top-right');
+                            alertify.success(res.message);
+
+                            $('#example').load(location.href + " #example");
+                        }
+                    }
+                });
+            }
+        });
+
+        //for image preview
+        function imagePreview(input) {
+            $('#imagePreviewContainer').css("display", "block");
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+
+                reader.onload = function (e) {
+                    $('#previewImage').attr('src', e.target.result);
+                }
+
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
     </script>
 </body>
 
